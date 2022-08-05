8d2944e1a47f4575dcbc25e183acc049b75cc05a
RainbowAfterRainE <107292295+RainbowAfterRainE@users.noreply.github.com>
2022-08-05 21:23:41 +0700

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,32 @@
+mySprite = sprites.create(img("""
+        . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . b 5 b . . . 
+            . . . . . . . . . b 5 b . . . . 
+            . . . . . . b b b b b b . . . . 
+            . . . . . b b 5 5 5 5 5 b . . . 
+            . b b b b b 5 5 5 5 5 5 5 b . . 
+            . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+            . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+            . . b d 5 5 b 1 f f 5 4 4 c . . 
+            b b d b 5 5 5 d f b 4 4 4 4 4 b 
+            b d d c d 5 5 b 5 4 4 4 4 4 b . 
+            c d d d c c b 5 5 5 5 5 5 5 b . 
+            c b d d d d d 5 5 5 5 5 5 5 b . 
+            . c d d d d d d 5 5 5 5 5 d b . 
+            . . c b d d d d d 5 5 5 b b . . 
+            . . . c c c c c c c c b b . . .
+    """),
+    SpriteKind.player)
+
+def on_forever():
+    if controller.left.is_pressed():
+        mySprite.set_velocity(-10, 0)
+    elif controller.right.is_pressed():
+        mySprite.set_velocity(10, 0)
+    elif controller.up.is_pressed():
+        mySprite.set_velocity(0, -10)
+    elif controller.down.is_pressed():
+        mySprite.set_velocity(0, 10)
+    else:
+        mySprite.set_velocity(0, 0)
+forever(on_forever)
